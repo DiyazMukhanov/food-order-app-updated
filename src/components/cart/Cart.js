b1a5312631114c76fee0657b0ee839bdcee57b2c
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-01-02 14:34:52 +0500

    Dev ongoing

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,14 +2,13 @@ import React from "react";
 
 const Cart = props => {
   return (
-      <React.Fragment>
+    
 <<<<<<< Updated upstream
-          <div></div>
-          <div></div>
+         
 =======
           
 >>>>>>> Stashed changes
-      </React.Fragment>
+      
   );
 };
 
