c8e292b86eb740a5d10fe2353650450bc6bfc190
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-01-12 22:15:55 +0500

    in process

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -8,6 +8,8 @@ import Checkout from "./Checkout";
 
 const Cart = props => {
   const [isCheckingOut, setIsCheckingOut] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [error, setError] = useState(null);
   const cartCtx = React.useContext(CartContext);
 
   const cartCloseHandler = () => {
@@ -19,26 +21,39 @@ const Cart = props => {
   }
 
   const requestSubmitHandler = async (userData) => {
-    const response = await fetch('https://food-app-react-342dd-default-rtdb.firebaseio.com/requests.json', {
-       method: 'POST',
-       body: JSON.stringify({
-         user: userData,
-         orderedItems: cartCtx.items
-       })
-    });
+    try{const response = await fetch('https://food-app-react-342dd-default-rtdb.firebaseio.com/requests.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+   });
+   if(!response.ok){
+     throw new Error('Ошибка отправки заказа');
+   }
+   setIsSubmitted(true);
+   
+  } catch(error){
+    setError(error.message);
   }
+  
+  }
+
+  const successMessage = <h1>Success</h1>;
 
   return (
      <Modal onClose = {props.onClose}>
          <div>
-             <CartList />
+         {!isSubmitted && <CartList />}
          </div>
-         {isCheckingOut && <Checkout onCancel = {cartCloseHandler} onConfirm = {requestSubmitHandler}/>}
+         {isCheckingOut && !isSubmitted && <Checkout onCancel = {cartCloseHandler} onConfirm = {requestSubmitHandler}/>}
+         
          <div className={classes.bottomCart}>
-           <div className={classes.topPart}>
+         {!isSubmitted && <div className={classes.topPart}>
              <h1 className={classes.h1}>К оплате</h1>
              <span className={classes.amount}>KZT {cartCtx.totalAmount.toFixed(2)}</span>
-             </div>
+             </div>}
+             {isSubmitted && <h2>Ваш заказ успешно отправлен... Спасибо за то, что выбрали нас!</h2>}
              {!isCheckingOut && 
              <div className={classes.buttonsBottom}>
              <button onClick={cartCloseHandler} className={classes.buttonClose} >Выйти</button> <button className={classes.buttonOrder} onClick = {checkOutHandler}>Заказать</button>
